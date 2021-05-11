76015aee1e049b76fe689fc8ec05f2a1feaedb9d
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2021-05-11 19:58:18 +0000

    Update script.min.js

--- classifier.py
+++ classifier.py
@@ -1,2 +1,16 @@
 
 import numpy
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import neighbors
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
+
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+
+def buildModel(dataset, method, parameters):
+    """
+    Build final model for predicting real testing data
+    """
