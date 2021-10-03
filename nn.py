79de47155de5a0505147d816eb7924e40d703bfb
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2021-10-03 15:41:37 +0000

    Bump version to 4.1.0

--- nn.py
+++ nn.py
@@ -24,4 +24,4 @@ def main():
 
     X_train = numpy.reshape(numpy.array(X_train), (X_train.shape[0], 1, X_train.shape[1]))
 
-    X_test = numpy.reshape(numpy.array(X_test), (X_test.shape[0], 1
+    X_test = numpy.reshape(numpy.array(X_test), (X_test.shape[0], 1, X_test.
