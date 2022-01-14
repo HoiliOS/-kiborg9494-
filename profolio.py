51403977ae021f33c52d3b37858cfbd3ce37c55f
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-01-14 15:48:18 +0000

    Added example

--- profolio.py
+++ profolio.py
@@ -18,4 +18,4 @@ class Profolio:
         return numpy.cumsum(self.profits)
 
     def annualSharpeRatio(self, n = 252):
-        return numpy.sqrt(n) * self.profits.mean() / self.profit
+        return numpy.sqrt(n) * self.profits.mean() / self.profits.std(
