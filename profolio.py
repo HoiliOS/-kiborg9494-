cad12eede326976bae2c3f8273569855550d50ea
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-01-28 13:49:21 +0000

    Update views.py

--- profolio.py
+++ profolio.py
@@ -26,4 +26,4 @@ def smart_trade(etf, method, delta):
     data = util.get_data(etf, '2014/1/1', '2016/12/31')
 
     # keep a copy for unscaled data for later gain calculation
-    # TODO re
+    # TODO replace by MinMax_S
