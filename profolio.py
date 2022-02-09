cb641c759fdce30929192b3bbdcd1e42b2d68f94
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-02-09 18:05:45 +0000

    Fix jshint warnings

--- profolio.py
+++ profolio.py
@@ -28,4 +28,4 @@ def smart_trade(etf, method, delta):
     # keep a copy for unscaled data for later gain calculation
     # TODO replace by MinMax_Scaler.inverse_transform()
     #
-    # the first day of test
+    # the first day of test is 2015/1
