db2793aaa8dcceb7b1346c08fa64497aab4b875e
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-04-04 08:33:57 +0000

    2005-09-30  H.J. Lu  <hongjiu.lu@intel.com>

--- profolio.py
+++ profolio.py
@@ -47,4 +47,4 @@ def smart_trade(etf, method, delta):
     #tr = dataMod[dataMod.index <= datetime.datetime(2016,06,30)]
     te = dataMod[dataMod.index > datetime.datetime(2015,12,30)]
     te = te[te.columns[0:-1]] # remove Up/Down label from testing
-    cl
+    clf = classifie
