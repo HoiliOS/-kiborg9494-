db482d549d3ac1f1a822bd0beef48256c5b0cf4d
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-04-23 13:07:52 +0000

    Fix intermittent test failure

--- profolio.py
+++ profolio.py
@@ -50,4 +50,4 @@ def smart_trade(etf, method, delta):
     clf = classifier.buildModel(tr, method, parameters)
 
     if method == 'RNN':
-        te = numpy.reshape(numpy.array(te), (te.shape[0
+        te = numpy.reshape(numpy.array(te), (te.shape[0], 1, te.
