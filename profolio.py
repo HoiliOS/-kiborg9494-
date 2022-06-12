54e72a7d0a5d32d73dfcfb14416b0e329e49d988
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-06-12 16:54:01 +0000

    copyright year

--- profolio.py
+++ profolio.py
@@ -60,4 +60,5 @@ def smart_trade(etf, method, delta):
         p = (test.Close[i+1] - test.Open[i+1]) / test.Open[i+1]
       else: # predict short
         p = -(test.Close[i+1] - test.Open[i+1]) / test.Open[i+1]
-      profi
+      profits[i] = p
+    r
