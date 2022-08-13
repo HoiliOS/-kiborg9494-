a8370c232bd3fe4e6ea9282cab241b16320701fd
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-08-13 17:43:50 +0000

    Update Defconfig

--- profolio.py
+++ profolio.py
@@ -78,4 +78,4 @@ def compareMethods():
         delta = best_delta[i]
         my = Profolio(name)
         for etf in ETFs:
-            my.profits += smart_trad
+            my.profits += smart_trade(etf, name, 
