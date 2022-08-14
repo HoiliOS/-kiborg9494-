f750449543f31704da4271f0a58ff8f385e6690d
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-08-14 16:13:50 +0000

    Base version

--- profolio.py
+++ profolio.py
@@ -79,4 +79,4 @@ def compareMethods():
         my = Profolio(name)
         for etf in ETFs:
             my.profits += smart_trade(etf, name, delta)
-        label, = plt.pl
+        label, = plt.plot(range(1, 2
