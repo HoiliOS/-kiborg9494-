d0e4eca3f6a4bcef4c24067f9ec9ca432a40228f
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-11-12 14:11:29 +0000

    scss

--- profolio.py
+++ profolio.py
@@ -103,4 +103,4 @@ def main():
     p1 = Profolio('SPY long-only')
     data = util.get_data('SPY', '2016/1/1', '2016/12/31')
     p1.profits = data.Return * 9
-    label, = plt.plot(range(1, 253), p
+    label, = plt.plot(range(1, 253), p1.accProfi
