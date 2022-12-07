5a4d89e658943d03098c808101f00b0a5cb57515
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-12-07 20:49:51 +0000

    7.6.0

--- profolio.py
+++ profolio.py
@@ -109,4 +109,5 @@ def main():
     # baseline2 All long-only
     p2 = Profolio('All long-only')
     for etf in ETFs:
-        data = util.get_data(etf, '2016/1/1', '2016/12/3
+        data = util.get_data(etf, '2016/1/1', '2016/12/31')
+        p2.
