1e093a9f7055ff55cadbcb2f2ad344f7c6b9d4bd
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2022-12-17 18:09:45 +0000

    Code cleaning...

--- profolio.py
+++ profolio.py
@@ -112,4 +112,4 @@ def main():
         data = util.get_data(etf, '2016/1/1', '2016/12/31')
         p2.profits += data.Return
     label, = plt.plot(range(1, 253), p2.accProfits(), 'g--', label=p2.name)
-    leg
+    legend.append
