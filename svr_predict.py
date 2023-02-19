9b4410322cde09917477c54df5c96cf8f6738e0a
HoiliOS <108049164+HoiliOS@users.noreply.github.com>
2023-02-19 06:51:46 +0000

    Added dockerfile

--- svr_predict.py
+++ svr_predict.py
@@ -10,3 +10,17 @@ import matplotlib.pyplot as plt
 class SVR_Solver:
 
     def __init__(self):
+
+        self.dates = []
+        self.prices = []
+
+    def get_data(self, filename):
+
+        with open(filename, 'r') as csvfile:
+            csvFileReader = csv.reader(csvfile)
+            next(csvFileReader)     # skipping column names
+            for row in csvFileReader:
+                self.prices.append(float(row[1]))
+
+        dates = range(1, len(self.prices)+1)
+        self.dates = np.reshape(dates, (len(dates), 1))
